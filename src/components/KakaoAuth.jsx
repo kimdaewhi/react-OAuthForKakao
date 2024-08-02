6c3c8fe6ecc5b76ec7dc363810256679348512fa
김대휘 <kimdaewhi@naver.com>
2024-08-02 15:30:54 +0900

    카카오 간편인증 개발 테스트

--- src/components/KakaoAuth.jsx
+++ src/components/KakaoAuth.jsx
@@ -1,9 +1,20 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import axios from 'axios'
 
 
 export default function KakaoAuth() {
     const rest_api_key = '634fee3fbd20fdea483fc61f1eaf17ba'
     const redirect_uri = 'http://localhost:9000/docs'
+    const [code, setCode] = useState(null)
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search)
+        const code = urlParams.get('code')
+        if(code) {
+            setCode(code);
+            getAccessToken(code)
+        }
+    }, []);
 
 
     const handleKakaoAuth = (event) => {
@@ -12,11 +23,26 @@ export default function KakaoAuth() {
         // 여기에 카카오 OAuth 인증을 구현하세요!
     }
 
+    const getAccessToken = async (code) => {
+        try {
+            const response = await axios.post('http://localhost:9000/auth_kakao/accessToken', {
+                grant_type: 'authorization_code',
+                client_id: rest_api_key,
+                redirect_uri: redirect_uri,
+                code: code
+            });
+            console.log('Access Token : ', response.data);
+        } 
+        catch(error) {
+            console.error('access token 발급 실패 : ', error)
+        }
+    }
+
 
     return (
         <div>
             <h1>OAuth 2.0</h1>
-            <a href={`http://localhost:9000/kakao/authorize?api_key=${rest_api_key}&redirect_uri=${redirect_uri}`} onClick={handleKakaoAuth}>
+            <a href={`http://localhost:9000/auth_kakao/authorize?api_key=${rest_api_key}&redirect_uri=${redirect_uri}`} onClick={handleKakaoAuth}>
                 <img height="30px" src={`${process.env.PUBLIC_URL}/images/kakao_login_small.png`} alt="Kakao Auth" />
             </a>
         </div>
